6686eb23d71eabc33d1deca9ea3cdb0485ec1481
sun_zw <sun_zw@neusoft.com>
2022-04-20 18:01:30 +0800

    这道题效率有点低
    Update RemoveNthNodeFromEndOfList.java

--- main/leetCode/MergeTwoSortedLists.java
+++ main/leetCode/MergeTwoSortedLists.java
@@ -17,20 +17,5 @@ public class MergeTwoSortedLists {
 
 
 // Definition for singly-linked list.
-class ListNode {
-    int val;
-    ListNode next;
 
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
 
---
+++ main/leetCode/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,48 @@
+package leetCode;
+
+import java.util.HashMap;
+
+public class RemoveNthNodeFromEndOfList {
+    public static void main(String[] args) {
+        RemoveNthNodeFromEndOfList r = new RemoveNthNodeFromEndOfList();
+        // r.removeNthFromEnd();
+    }
+
+    // 自己想的方法
+    // public ListNode removeNthFromEnd(ListNode head, int n) {
+    //     HashMap<Integer, ListNode> map = new HashMap<>();
+    //     ListNode headCopy = head;
+    //     int nth = 1;
+    //     while (head != null) {
+    //         map.put(nth, head);
+    //         head = head.next;
+    //         nth++;
+    //     }
+    //     ListNode pre = map.get(nth - n - 1);
+    //     ListNode next = map.get(nth - n + 1);
+    //     if (pre == null) return next;
+    //     pre.next = next;
+    //     return headCopy;
+    // }
+
+    // 抄的递归回溯法
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        int traverse = traverse(head, n);
+        if(traverse == n)
+            return head.next;
+        return head;
+    }
+
+    private int traverse(ListNode node, int n) {
+        if(node == null)
+            return 0;
+        int num = traverse(node.next, n);
+        if(num == n)
+            node.next = node.next.next;
+        return num + 1;
+    }
+
+}
+
+
+
