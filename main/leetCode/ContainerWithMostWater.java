89dc0603fae24796da4563355ade42436f7cbfcd
sun_zw <sun_zw@neusoft.com>
2022-04-21 18:01:51 +0800

    Create ContainerWithMostWater.java

---
+++ main/leetCode/ContainerWithMostWater.java
@@ -0,0 +1,23 @@
+package leetCode;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class ContainerWithMostWater {
+    public static void main(String[] args) {
+        ContainerWithMostWater c = new ContainerWithMostWater();
+        final int i = c.maxArea(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 999, 999, 10, 11});
+        System.out.println(i);
+    }
+
+    public int maxArea(int[] height) {
+        int ans = 0;
+        for (int i = 0; i < height.length - 1; i++) {
+            for (int j = i + 1; j < height.length; j++) {
+                ans = Math.max(ans, height[i] * height[j]);
+            }
+        }
+        return ans;
+    }
+}
