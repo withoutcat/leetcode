afe92e3a72b6f952ebb114d5fc6f23275c5840c6
sunziwen <sunziwens@gmail.com>
2023-03-05 17:45:42 +0800

    改用了treeMap时间复杂的比想象中要低 https://leetcode.cn/problems/merge-k-sorted-lists/submissions/

--- src/main/kotlin/MergeKLists.kt
+++ src/main/kotlin/MergeKLists.kt
@@ -1,13 +1,16 @@
+import java.util.*
+
 fun mergeKLists(lists: Array<ListNode?>): ListNode? {
-    val dataMap = LinkedHashMap<Int, Int>()
+    val dataMap = TreeMap<Int, Int>()
+    fun plus(key: Int) {
+        dataMap[key] = (dataMap[key] ?: 0) + 1
+    }
     // 统计所有元素的数量
     var count = 0
     lists.forEach {
         var tmpNode: ListNode? = it
         while (tmpNode != null) {
-            dataMap[tmpNode.`val`] =
-                if (dataMap.contains(tmpNode.`val`)) dataMap[tmpNode.`val`]!! + 1
-                else 1
+            plus(tmpNode.`val`)
             tmpNode = tmpNode.next
             count++
         }
@@ -15,26 +18,19 @@ fun mergeKLists(lists: Array<ListNode?>): ListNode? {
     // 遍历输出结果
     var node: ListNode? = null
     var head: ListNode? = null
-    // dataMap的key无法保证自然排序，如果使用treeMap虽然能保证有序，但本质上就是暴力解了，因为每次插入元素时间复杂度都是O(logn)
-    // 我认为这里直接从0开始按升序遍历
-    var i = 0
-    while (count > 0) {
-        dataMap[i]?.let { `val` ->
-            // 如果i重复出现就链接起来
-            for (j in 0 until `val`) {
-                node = if (head == null) {
-                    head = ListNode(i)
-                    head
-                } else {
-                    node?.let { n ->
-                        n.next = ListNode(i)
-                        n.next
-                    }
+    dataMap.forEach {
+        // 如果i重复出现就链接起来
+        for (j in 0 until it.value) {
+            node = if (head == null) {
+                head = ListNode(it.key)
+                head
+            } else {
+                node?.let { n ->
+                    n.next = ListNode(it.key)
+                    n.next
                 }
             }
-            count -= `val`
         }
-        i++
     }
     return head
 }
--- src/test/kotlin/MergeKLists.kt
+++ src/test/kotlin/MergeKLists.kt
@@ -7,16 +7,24 @@ class MergeKLists {
     fun mergeKListsTest() {
         // 输入：lists = [[1,4,5],[1,3,4],[2,6]]
         // 输出：[1,1,2,3,4,4,5,6]
-        val lists = arrayOf(
+        var lists = arrayOf(
             makeNode(1, 4, 5),
             makeNode(1, 3, 4),
             makeNode(2, 6)
         )
 
-        val node = mergeKLists(lists)
-        val nodeList = readNode(node)
+        var node = mergeKLists(lists)
+        var nodeList = readNode(node)
         assertContentEquals(listOf(1, 1, 2, 3, 4, 4, 5, 6), nodeList)
         // [[2],[],[-1]] 这个用例没过
+        lists = arrayOf(
+            makeNode(2),
+            makeNode(),
+            makeNode(-1)
+        )
+        node = mergeKLists(lists)
+        nodeList = readNode(node)
+        assertContentEquals(listOf(-1, 2), nodeList)
     }
 
     private fun readNode(head: ListNode?): List<Int> {
